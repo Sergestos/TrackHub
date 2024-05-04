12bfa4f7fbd3ee397a3aec9bea5023a243086c57
shauf <shauf@gmail.com>
2024-05-05 02:35:57 +0300

    select and remove actions

--- App/trackhub/src/app/spa/commit/commit.component.ts
+++ App/trackhub/src/app/spa/commit/commit.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { ExerciseModel } from './commit.models';
+import { ExerciseComponent } from './exercise/exercise.component';
 
 @Component({
 	selector: 'trh-commit',
 	templateUrl: './commit.component.html',
 	styleUrls: ['./commit.component.css']
 })
-export class CommitComponent {
-	public totalEcounter: number = 0;
+export class CommitComponent {	
+	public isUseTodaysDate: boolean = true;
 	public selectedDate: Date = new Date();
 
-	public exercises: ExerciseModel[] = 
-	[
-		{
-			id: null,
-			song: 'random'
-		}
-	];
+	public exercises: ExerciseModel[] = [];
+
+	@ViewChildren(ExerciseComponent) exerciseViews!: QueryList<ExerciseComponent>;
+
+	//TODO remove 
+	private tempCounter = 1;
 
 	public onAddClick(): void {
 		this.exercises.push({
-			id: null,
+			id: this.tempCounter++,
 			song: 'random'
 		});
+	}
+
+	public onRemoveClick(): void {
+		var exercisesToRemove = this.exerciseViews.filter(x => x.isSelected).map(x => x.model.id);
+		this.exercises = this.exercises.filter(x => !exercisesToRemove.includes(x.id));
+	}
+
+	public onAllSelectedChanged(event: any): void {		
+		this.exerciseViews.forEach(x => x.toggleIsSelected(event.target.checked));
 	}
 }
--- App/trackhub/src/app/spa/commit/exercise/exercise.component.ts
+++ App/trackhub/src/app/spa/commit/exercise/exercise.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ExerciseModel } from '../commit.models';
 
 @Component({
     selector: 'trh-exercise',
     templateUrl: './exercise.component.html',
     styleUrls: ['./exercise.component.css']
-  })
-  export class ExerciseComponent {
-    
-  }
-  
+})
+export class ExerciseComponent {
+
+    @Input()
+    public model!: ExerciseModel;
+    public isSelected: boolean = false;
+
+    constructor() {
+
+    } 
+
+    public toggleIsSelected(value: boolean): void {
+        this.isSelected = value;
+    }
+}
