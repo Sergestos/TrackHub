dc0247ccf6910654d9116fc4c33aaf1f9b3d6a5c
shauf <shauf@gmail.com>
2024-10-06 03:12:57 +0300

    Refactor exercise page with Tailwind

--- App/trackhub/src/app/moduls/exericse-list/details-exercise-card/details-exercise-card.component.ts
+++ App/trackhub/src/app/moduls/exericse-list/details-exercise-card/details-exercise-card.component.ts
@@ -1,23 +1,20 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { ExerciseListService } from "../../../providers/services/exercise-list.service";
 import { Observable, of } from "rxjs";
 import { RecordDetailsItem } from "../exercise-list.models";
 
 @Component({
-	selector: 'trh-details-exercise-card',
+	selector: 'trackhub-details-exercise-card',
 	templateUrl: './details-exercise-card.component.html',
 	styleUrls: ['./details-exercise-card.component.css']
 })
-export class DetailsExerciseItemComponent implements OnInit {	
-	@Input() 
+export class DetailsExerciseItemComponent implements OnInit {
+	@Input()
 	public exerciseId!: string;
 
 	@Input()
 	public exerciseDetailsModels?: RecordDetailsItem[] | null;
 
 	public exerciseDetails$: Observable<RecordDetailsItem[]> = new Observable<RecordDetailsItem[]>();
-
-	constructor(private exerciseListService: ExerciseListService) { }
 
 	public ngOnInit(): void {
 		if (this.exerciseDetailsModels) {
