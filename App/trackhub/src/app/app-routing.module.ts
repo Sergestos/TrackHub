b7bf40caba00ee72356cf095107761bc62057d29
Shauf <Shauf1996@gmail.com>
2025-12-06 21:42:57 +0200

    Add nav, small interface changes

--- App/trackhub/src/app/app-routing.module.ts
+++ App/trackhub/src/app/app-routing.module.ts
@@ -5,17 +5,17 @@ import { AuthGuard } from './providers/guards/auth.guard';
 export const routes: Routes = [
   {
     path: 'app/list',
-    loadChildren: () => import('./moduls/exercise-list/exercise-list.module').then(m => m.ExerciseListModule),
+    loadChildren: () => import('./modules/exercise-list/exercise-list.module').then(m => m.ExerciseListModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'app/commit',
-    loadChildren: () => import('./moduls/commit/commit.module').then(m => m.CommitModule),
+    loadChildren: () => import('./modules/commit/commit.module').then(m => m.CommitModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'app/login',
-    loadChildren: () => import('./moduls/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: '', redirectTo: 'app/list', pathMatch: 'full'
