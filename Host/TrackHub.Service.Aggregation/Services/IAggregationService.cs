f9c8ac03b0306ed151a674a456aac76b32c83953
Shauf <Shauf1996@gmail.com>
2025-12-21 21:37:36 +0200

    Remove unused service

--- Host/TrackHub.Service.Aggregation/Services/IAggregationService.cs
+++
@@ -1,8 +0,0 @@
-﻿using TrackHub.Messaging.Aggregations;
-
-namespace TrackHub.Service.Aggregation.Services;
-
-public interface IAggregationService
-{
-    Task SendAggregationAsync(AggregationEventMessage payload, CancellationToken cancellationToken = default);
-}
