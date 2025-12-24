566c755a5203451a2e13397db7a142db10aaf7e8
Shauf <Shauf1996@gmail.com>
2025-12-24 21:45:31 +0200

    Fix redirect on app reload

--- App/trackhub/src/app/modules/auth/components/auth.component.ts
+++ App/trackhub/src/app/modules/auth/components/auth.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, effect, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 import { SocialUser } from "@abacritt/angularx-social-login";
 import { tap } from "rxjs";
 import { AuthService } from "../../../providers/services/auth.service";
@@ -16,6 +16,17 @@ export class AuthComponent implements OnInit {
   private authService = inject(AuthService);
   private loadingService = inject(LoadingService);
   private router = inject(Router);
+  private activeRoute = inject(ActivatedRoute);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.isAuthorized()) {
+        this.activeRoute.queryParams.subscribe(params => {
+          this.router.navigateByUrl(params['redirect']);
+        });
+      }
+    })
+  }
 
   public ngOnInit(): void {
     this.authService
@@ -23,7 +34,7 @@ export class AuthComponent implements OnInit {
       .subscribe((user: SocialUser) => {
         if (!user)
           return;
-        
+
         this.loadingService.show();
         this.authService.authExternalUser(user)
           .pipe(tap(_ => this.loadingService.complete()))
